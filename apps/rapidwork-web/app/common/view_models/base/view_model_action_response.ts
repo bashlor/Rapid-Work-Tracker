795b7868eade30a8a3e40f9d010b61a3e6393757
Elie Patrice <elie.patrice@pm.me>
2025-09-14 13:51:08 +0200

    chore: typo in Update view_model_action_response.ts

--- apps/rapidwork-web/app/common/view_models/base/view_model_action_response.ts
+++ apps/rapidwork-web/app/common/view_models/base/view_model_action_response.ts
@@ -2,7 +2,7 @@
  * Base class for ViewModel Action Responses
  * Encapsulates the input parameters, feature response, and formatted output
  */
-export abstract class ViewModelActionResponse<TInput, TEntity, TOutput> {
+export abstract class ViewModelActionResponse<TInput, TEntity> {
   constructor(
     protected readonly input: TInput,
     protected readonly entities: TEntity
@@ -12,5 +12,5 @@ export abstract class ViewModelActionResponse<TInput, TEntity, TOutput> {
    * Returns the formatted and serialized data to be returned by the controller
    * This method should transform the domain entities into the appropriate format for the HTTP response
    */
-  abstract publicHttpJsonResponse(...args: any[]): TOutput
+  abstract publicHttpJsonResponse(...args: any[]): any
 }
