d6eaf0daec6d01985493a3b3f547f23af7f1d1c2
Elie Patrice <elie.patrice@pm.me>
2025-09-14 13:32:55 +0200

    chore: remove unused dtos

--- apps/rapidwork-web/app/dtos/session_dto.ts
+++
@@ -1,9 +0,0 @@
-export interface SessionHttpDto {
-  description: null | string
-  duration: null | number
-  endTime: null | string
-  id: string
-  startTime: string
-  taskId: string
-  userId: string
-}
